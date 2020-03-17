81abaf1c7e624d0d71ae5e0b0f8f5e7d54e9e87e
Danielle Demke <danielledemke@gmail.com>
2020-03-17 15:24:30 -0500

    Init

---
+++ SuperheroProject/Models/Superhero.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperheroProject.Models
+{
+    public class Superhero
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Alterego { get; set; }
+        public string Ability { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+
+    }
+}
